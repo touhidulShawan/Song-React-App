5a3d921382119a6ebfbbc308a2e7a3a8005bfc7e
touhidulShawan <touhidulislamshaon@gmail.com>
2019-02-11 19:40:13 +0600

    Updates

---
+++ precache-manifest.d252a0837c228e6c65e313ad7db81fba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f1ad41d834fc605cb87",
+    "url": "/Song-React-App/static/js/runtime~main.7f1ad41d.js"
+  },
+  {
+    "revision": "06d417d75b70f35a19ea",
+    "url": "/Song-React-App/static/js/main.06d417d7.chunk.js"
+  },
+  {
+    "revision": "03f2b03905ff4a6d1cae",
+    "url": "/Song-React-App/static/js/2.03f2b039.chunk.js"
+  },
+  {
+    "revision": "06d417d75b70f35a19ea",
+    "url": "/Song-React-App/static/css/main.7ece6c3a.chunk.css"
+  },
+  {
+    "revision": "441b6a8fefc20bbf71ba355e7cb22700",
+    "url": "/Song-React-App/index.html"
+  }
+];
